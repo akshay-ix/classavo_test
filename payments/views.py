c6dcebd487486756e66bcbb78fdd83a496ae501b
akshay-ix <akshay@classavo.com>
2019-05-29 15:29:47 -0400

    charge fuction skeleton ready

---
+++ payments/views.py
@@ -0,0 +1,29 @@
+import stripe
+
+from django.shortcuts import render
+from django.conf import settings  # new
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+stripe.api_key = settings.STRIPE_SECRET_KEY  # new
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):  # new
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        return context
+
+
+def charge(request):  # new
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=500,
+            currency='usd',
+            description='A Django charge',
+            source=request.POST['stripeToken']
+        )
+        return render(request, 'charge.html')
